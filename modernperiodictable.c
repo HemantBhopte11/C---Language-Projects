702474e5f3e370b05dece13ca68912fec2fd93c0
Hemant Bhopte <hemantbhopte2006@gmail.com>
2026-01-12 12:33:35 +0530

    Done

---
+++ modernperiodictable.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    char *elements[118] = {
+        "H", "He",
+        "Li", "Be", "B", "C", "N", "O", "F", "Ne",
+        "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar",
+        "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+        "Ga", "Ge", "As", "Se", "Br", "Kr",
+        "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd",
+        "In", "Sn", "Sb", "Te", "I", "Xe",
+        "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy",
+        "Ho", "Er", "Tm", "Yb", "Lu",
+        "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg",
+        "Tl", "Pb", "Bi", "Po", "At", "Rn",
+        "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf",
+        "Es", "Fm", "Md", "No", "Lr",
+        "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn",
+        "Nh", "Fl", "Mc", "Lv", "Ts", "Og"
+    };
+
+    printf("\nModern Periodic Table (118 Elements)\n");
+    printf("====================================\n\n");
+
+    for(int i = 0; i < 118; i++)
+    {
+        printf("%-4s", elements[i]);
+
+        if((i + 1) % 10 == 0)
+            printf("\n");
+    }
+
+    printf("\n");
+
+    return 0;
+}
